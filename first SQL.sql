497f250803614af0df241aa2c9fdbab8489cd4bf
Tenneri Rohit <134133259+Tennerirohit@users.noreply.github.com>
2026-01-05 17:16:42 +0530

    First SQL Query

---
+++ first SQL.sql
@@ -0,0 +1,8 @@
+create database college;
+use college;
+create table student(id INT PRIMARY KEY,name VARCHAR (50),age INT NOT NULL);
+insert into student values (1,"Sai",99);
+insert into student values (2,"Rohit",23);
+insert into student values (3,"Dega",250);
+insert into student values (4,"Laya",40);
+select * from student;
