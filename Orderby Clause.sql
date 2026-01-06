f16756a6c6187b2be4e3f747665f7e045abb3f13
Tenneri Rohit <134133259+Tennerirohit@users.noreply.github.com>
2026-01-06 11:52:20 +0530

    ORDER BY  Clause

---
+++ Orderby Clause.sql
@@ -0,0 +1,17 @@
+use college;
+create table student(rollno int primary key , name varchar(50), marks int not null, grade varchar(1),city varchar (20));
+insert into student (rollno,name,marks,grade,city) values (101,"anil",78,"C","Pune"),
+(102,"bhumika",93,"A","Mumbai"),
+(103,"chetan",85,"B","Mumbai"),
+(104,"dhruv",96,"A","Delhi"),
+(105,"emanuel",12,"F","Delhi"),
+(106,"farah",82,"B","Delhi");
+
+select * from student
+where city in ("Mumbai")
+order by marks desc
+limit 3;
+
+select * from student
+order by marks asc
+limit 4;
