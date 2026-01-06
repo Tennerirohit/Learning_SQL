0db86582c9d3fcd9954742e9a58dbbda2ab5af77
Tenneri Rohit <134133259+Tennerirohit@users.noreply.github.com>
2026-01-06 12:06:36 +0530

    GROUP BY clause

---
+++ GROUP BY Clause.sql
@@ -0,0 +1,22 @@
+#Group by clause
+
+use college;
+create table student(rollno int primary key , name varchar(50), marks int not null, grade varchar(1),city varchar (20));
+insert into student (rollno,name,marks,grade,city) values (101,"anil",78,"C","Pune"),
+(102,"bhumika",93,"A","Mumbai"),
+(103,"chetan",85,"B","Mumbai"),
+(104,"dhruv",96,"A","Delhi"),
+(105,"emanuel",12,"F","Delhi"),
+(106,"farah",82,"B","Delhi");
+
+select city 
+from student
+group by city;
+
+select city , count(name)
+from student
+group by city;
+
+select city,name ,count(rollno)
+from student
+group by rollno;
